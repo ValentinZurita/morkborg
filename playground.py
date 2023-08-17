ef79e5297b81fe70457e746a162843132bf8b752
Valentin Alejandro <valentin.alejandro@outlook.com>
2023-08-16 18:41:33 -0600

    Merge roll_abilities into master

--- playground.py
+++ playground.py
@@ -1,14 +1,10 @@
 from Dice import Dice
-from character import Character
+from Character import Character
 
 
 
 player = Character('Paco')
-print("Agility:" + str(player.agility))
-print("toughness:" + str(player.toughness))
-print("presence:" + str(player.presence))
-print("strenght:" + str(player.strength))
-print("Inventory:" + str(player.carrying_capacity))
 
 player.set_random_equipment()
 player.show_inventory()
+player.show_abilities()
